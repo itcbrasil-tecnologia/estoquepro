9aec751ed7a4fe136ed179e67323ac1ceb2c7abf
null <null>
2025-07-12 00:42:48 -0300

    v22

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'placehold.co',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'firebasestorage.googleapis.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
